6afded5446346274ba6d216c11b5d515c823521a
Leandro Gabriel Villalba <villalbaleandrogabriel@gmail.com>
2022-11-25 16:20:18 -0300

    cmodificacion de vista y creacion de forms de registro

---
+++ ProyectoTwitter/AppTwitter/forms.py
@@ -0,0 +1,8 @@
+from django.forms import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+
+class Registro(UserCreationForm):
+    class Meta:
+        model = User
+        fields = ['first_name', 'username','email','password1', 'password2']
--- ProyectoTwitter/AppTwitter/views.py
+++ ProyectoTwitter/AppTwitter/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from .models import Posteo, Usuario
+from .forms import Registro
 
 # Create your views here.
 def inicio(request):
     posteos = Posteo.objects.all()
     return render(request, '01_inicio-twitter.html', {'posteos': posteos})
+
