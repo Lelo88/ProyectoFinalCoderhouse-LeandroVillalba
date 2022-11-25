dea7145a8593d8fc76cd02c750ac5901d655f7a3
Leandro Gabriel Villalba <villalbaleandrogabriel@gmail.com>
2022-11-25 20:26:23 -0300

    creacion definitiva de login y logout con sus respectivas plantillas y redireccionamiento a traves de settings

--- ProyectoTwitter/AppTwitter/urls.py
+++ ProyectoTwitter/AppTwitter/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
+from django.contrib.auth.views import LoginView, LogoutView
 from .views import inicio, registro
+
 
 urlpatterns = [
     path('', inicio, name = 'Inicio'),
-    path('registro/', registro, name = 'Registro')
+    path('registro/', registro, name = 'Registro'),
+    path('login/', LoginView.as_view(template_name='04_login.html'), name='Login'),
+    path('login/', LogoutView.as_view(), name='Logout')
 ]
