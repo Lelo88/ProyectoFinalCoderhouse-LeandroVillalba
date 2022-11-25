7dd630ecf8f2dcb71dfa3e598cf5229ac04fa286
Leandro Gabriel Villalba <villalbaleandrogabriel@gmail.com>
2022-11-24 23:51:21 -0300

    creo de template de inicio y carga de algunas imagenes

--- ProyectoTwitter/AppTwitter/views.py
+++ ProyectoTwitter/AppTwitter/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from .models import Posteo, Usuario
 
 # Create your views here.
+def inicio(request):
+    posteos = Posteo.objects.all()
+    return render(request, '01_inicio-twitter.html', {'posteos': posteos})
