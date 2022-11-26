20e68d967bfac3431e573322fbc806c18c22692e
Leandro Gabriel Villalba <villalbaleandrogabriel@gmail.com>
2022-11-25 22:19:52 -0300

    creacion de posteos de usuario en su inicio

--- ProyectoTwitter/AppTwitter/forms.py
+++ ProyectoTwitter/AppTwitter/forms.py
@@ -1,4 +1,5 @@
-from django.forms import forms
+from django import forms
+from .models import Posteo
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 
@@ -6,3 +7,11 @@ class Registro(UserCreationForm):
     class Meta:
         model = User
         fields = ['first_name', 'last_name', 'username','email','password1', 'password2']
+        
+class FormularioPosteo(forms.ModelForm):
+    contenido = forms.CharField(widget=forms.Textarea(attrs={'class':'form-control w-100', 'id':'contentsBox',
+                                                            'rows':'3', 'placeholder':'¿Qué está pasando?'}))
+    
+    class Meta:
+        model = Posteo
+        fields = ['contenido']
--- ProyectoTwitter/AppTwitter/views.py
+++ ProyectoTwitter/AppTwitter/views.py
@@ -1,11 +1,22 @@
 from django.shortcuts import render, redirect
 from .models import Posteo, Usuario
-from .forms import Registro
+from .forms import Registro, FormularioPosteo
 
 # Create your views here.
 def inicio(request):
     posteos = Posteo.objects.all()
-    return render(request, '01_inicio-twitter.html', {'posteos': posteos})
+    if request.method == 'POST':
+        formulario_posteo = FormularioPosteo(request.POST)
+        if formulario_posteo.is_valid():
+            posteo = formulario_posteo.save(commit=False)
+            posteo.usuario_posteo = request.user
+            posteo.save()
+            return redirect('Inicio')
+    else:
+        formulario_posteo = FormularioPosteo()
+    
+    contexto = {'posteos': posteos, 'formulario': formulario_posteo}
+    return render(request, '01_inicio-twitter.html', contexto)
 
 def registro(request):
     if request.method == 'POST':
