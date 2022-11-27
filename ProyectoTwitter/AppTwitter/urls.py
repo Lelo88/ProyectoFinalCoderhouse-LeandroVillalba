3b9693e600f92fd87c60d75415a45c37851ffa48
Leandro Gabriel Villalba <villalbaleandrogabriel@gmail.com>
2022-11-27 10:31:01 -0300

    creacion de carpeta media para la vista de avatars

--- ProyectoTwitter/AppTwitter/urls.py
+++ ProyectoTwitter/AppTwitter/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from django.contrib.auth.views import LoginView, LogoutView
+from django.conf import settings
+from django.conf.urls.static import static
 from .views import borrar_tweet, inicio, registro
+
 
 
 urlpatterns = [
@@ -9,4 +12,4 @@ urlpatterns = [
     path('login/', LoginView.as_view(template_name='04_login.html'), name='Login'),
     path('login/', LogoutView.as_view(), name='Logout'),
     path('borrado/<int:id_posteo>', borrar_tweet, name='BorrarTweet')
-]
+] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
